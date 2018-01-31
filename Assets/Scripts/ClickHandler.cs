92cf02bd9225549ad55b9fddc2d8120721f93a0a
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-01-31 18:16:07 -0500

    Finished week 2

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+    /*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    // Input
+	void Update () {
+		
+	}
+
+    // Guarenteed to run at a specific interval
+    // Physics (i.e. things that are time dependent)
+    void FixedUpdate()
+    {
+
+    }
+    */
+
+    public GameObject captainFalcon;
+    public Sprite firstImage;
+    public Sprite secondImage;
+
+    private Rigidbody2D rBody;
+    private SpriteRenderer sRend;
+    
+    void Start()
+    {
+        // Initialization of component references
+        rBody = captainFalcon.GetComponent<Rigidbody2D>();
+        sRend = captainFalcon.GetComponent<SpriteRenderer>();
+
+        rBody.velocity = new Vector2(5, 0);
+    }
+
+    void Update()
+    {
+
+    }
+    
+    // Executes when a mouse down event occurs on an object with this script.
+    void OnMouseDown()
+    {
+        if(sRend.sprite == firstImage)
+        {
+            sRend.sprite = secondImage;
+        } else
+        {
+            sRend.sprite = firstImage;
+        }
+
+        rBody.velocity *= -1;
+    }
+}

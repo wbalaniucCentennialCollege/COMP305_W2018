36dc604e76b9fada633bc26df9ae5329bcfeca5d
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-02-07 18:30:58 -0500

    Added animation examples

---
+++ Assets/Scripts/ExplosionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionHandler : MonoBehaviour {
+
+    public GameObject scoreIndicator;
+
+    void Explode()
+    {
+        Instantiate(scoreIndicator, this.transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}

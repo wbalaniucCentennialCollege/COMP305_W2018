a0a4068ea73ea403e44252ae019d73ecd17f49bf
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-02-14 18:07:09 -0500

    Kirby Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,17 @@ public class PlayerController : MonoBehaviour {
 
     // Public variables
     public float maxSpeed = 10.0f;
+    public float jumpForce = 300.0f;
+    public Transform groundCheck;
+    public LayerMask defineGround;
 
     // Private variables
     private Rigidbody2D rBody;
     private SpriteRenderer sRend;
     private Animator animator;
+
+    private bool isGrounded = false;
+    private float groundRadius = 0.2f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +24,25 @@ public class PlayerController : MonoBehaviour {
         sRend = this.GetComponent<SpriteRenderer>();
         animator = this.GetComponent<Animator>();
 	}
+
+    void Update()
+    {
+        if (Input.GetAxis("Jump") > 0 && isGrounded)
+        {
+            animator.SetBool("Ground", isGrounded);
+            rBody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
 	
 	// Used for physics calculations
 	void FixedUpdate () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, defineGround);
+        // Debug.Log("Grounded? " + isGrounded);
+        animator.SetBool("Ground", isGrounded);
+
+        animator.SetFloat("vSpeed", rBody.velocity.y);
+
         float moveHoriz = Input.GetAxis("Horizontal");
 
         // Pass horizontal velocity to animator (SPEED)
